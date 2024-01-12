b20246a4a274406ef2cb77e570280f83a32b1702
Sclavo Morgan <132832713+ElChip21@users.noreply.github.com>
2024-01-12 11:28:15 +0100

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,68 @@
+<!-- ROW -->
+</div>
+</main>
+
+<footer class="bg-dark page-footer font-small blue pt-4">
+
+    <div class="container-fluid text-center text-md-left">
+
+        <div class="row">
+            <div class="col-md-6 mt-md-0 mt-3">
+                <h5 class="text-white text-uppercase">Footer Content</h5>
+                <p class="text-white">Welcome to our super online shop !</p>
+            </div>
+            <hr class="clearfix w-100 d-md-none pb-3">
+            <div class="col-md-3 mb-md-0 mb-3">
+                <h5 class="text-white text-uppercase">Links</h5>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#" class="text-white"> Our super shirt</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-white"> Our trendy t-shirt</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-white"> Pullover for winter</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 mb-md-0 mb-3">
+                <h5 class="text-white text-uppercase">Links</h5>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="contact.php" class="text-white"> Contact</a>
+                    </li>
+                    <li>
+                        <a href="index.php" class="text-white"> Shop</a>
+                    </li>
+                    <li>
+                        <a href="cart.php" class="text-white"> Cart</a>
+                    </li>
+                    <li>
+                        <a href="profile.php" class="text-white"> My profile</a>
+                    </li>
+                    <li>
+                        <a href="connection.php" class="text-white"> Connection</a>
+                    </li>
+                    <li>
+                        <a href="registration.php" class="text-white"> Registration</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-white footer-copyright text-center py-3">
+        © 2021 Copyright
+        <a href="">Bootstrap online shop</a>
+    </div>
+</footer>
+
+<!-- container-fluid -->
+</div>
+
+<script src="js/app.js"></script>
+</body>
+
+</html>
